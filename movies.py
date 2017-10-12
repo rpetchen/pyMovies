24c6f21e679887c97901fb161f29a3a881a826ed
rpetchen <rpetchen@gmu.edu>
2017-10-12 16:37:34 -0400

    Updated with feedback

--- movies.py
+++ movies.py
@@ -1,7 +1,10 @@
 import webbrowser
+
+
+# Movie class used to create an instance of a movie with a title, storlyine, poster, and trailer # NOQA
 class Movie():
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+    	         poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -9,4 +12,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
